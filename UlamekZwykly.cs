58dd8cb95e25bc06e4b72de6ed7eb6a7eb2860f9
BlokerX <67635977+BlokerX@users.noreply.github.com>
2021-09-26 18:18:08 +0200

    Update UlamekZwykly.cs

--- UlamekZwykly.cs
+++ UlamekZwykly.cs
@@ -6,21 +6,31 @@ using System.Threading.Tasks;
 
 namespace Operators
 {
+#pragma warning disable CS0660 // Typ definiuje operator == lub !=, ale nie przesłania metody Object.Equals(object o)
+#pragma warning disable CS0661 // Typ definiuje operator == lub !=, ale nie przesłania metody Object.GetHashCode()
     public class UlamekZwykly
+#pragma warning restore CS0661 // Typ definiuje operator == lub !=, ale nie przesłania metody Object.GetHashCode()
+#pragma warning restore CS0660 // Typ definiuje operator == lub !=, ale nie przesłania metody Object.Equals(object o)
     {
         public UlamekZwykly(int licznik, int mianownik)
         {
+            if (mianownik == 0)
+                throw MianownikToZero;
             Licznik = licznik;
             Mianownik = mianownik;
         }
 
-        public int Licznik { get; set; }
-        public int Mianownik { get; set; }
+        public int Licznik { get; set; } = 1;
+        public int Mianownik { get { return mianownik; } set { if (value == 0) throw MianownikToZero; mianownik = value; } }
+        private int mianownik = 1;
+        public float WartoscCalkowita() { return Licznik / Mianownik; }
 
-        public float WartoscCalkowita()
-        {
-            return Licznik / Mianownik;
-        }
+        /// <summary>
+        /// Wyjątek występuje gdy mianownik osiągnął wartość 0
+        /// </summary>
+        public static Exception MianownikToZero = new Exception("Mianownik wynosi zero");
+
+        #region Skracanie ułamka
 
         /// <summary>
         /// Skracanie ułamka
@@ -47,6 +57,8 @@ namespace Operators
             return Skroc(this);
         }
 
+        #endregion
+
         #region Operatory_i_przeciążenia
 
         // Operacje na ułamku:
@@ -56,6 +68,17 @@ namespace Operators
         public static UlamekZwykly operator -(UlamekZwykly a)
             => new UlamekZwykly(-a.Licznik, a.Mianownik);
 
+
+        // Działania na ułamkach i liczbach typu int:
+
+        public static UlamekZwykly operator +(UlamekZwykly u, int i)
+            => Skroc(u + new UlamekZwykly(i, 1));
+        public static UlamekZwykly operator -(UlamekZwykly u, int i)
+            => Skroc(u - new UlamekZwykly(i, 1));
+        public static UlamekZwykly operator *(UlamekZwykly u, int i)
+            => Skroc(u * new UlamekZwykly(i, 1));
+        public static UlamekZwykly operator /(UlamekZwykly u, int i)
+            => Skroc(u / new UlamekZwykly(i, 1));
 
         // Działania na ułamkach:
 
@@ -122,6 +145,37 @@ namespace Operators
 
         // ToString():
         public override string ToString() => $"{Licznik} / {Mianownik}";
+
+        public static UlamekZwykly UlamekZInta(int i)
+        {
+            return new UlamekZwykly(i, 1);
+        }
+
+        public static UlamekZwykly UlamekZFloat(float f)
+        {
+            int dzielnik = 1;
+
+            while (f % 1 != 0)
+            {
+                f = f * 10;
+                dzielnik = dzielnik * 10;
+            }
+
+            return new UlamekZwykly((int)f, dzielnik);
+        }
+        
+        public static UlamekZwykly UlamekZDouble(double d)
+        {
+            int dzielnik = 1;
+
+            while (d % 1 != 0)
+            {
+                d = d * 10;
+                dzielnik = dzielnik * 10;
+            }
+
+            return new UlamekZwykly((int)d, dzielnik);
+        }
 
         #endregion
 
